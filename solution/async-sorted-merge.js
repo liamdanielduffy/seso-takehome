3081c23ebbfd696b3d066063ee8b5e1ed4f85c6e
Liam Duffy <liamdanielduffy@gmail.com>
2024-08-29 12:50:27 -0400

    solutions

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,81 @@
 "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+const { Subject, merge } = require("rxjs");
+const { scan } = require("rxjs/operators");
+const { MinPriorityQueue } = require("@datastructures-js/priority-queue");
 
-module.exports = (logSources, printer) => {
+function mergeAndPrintLogs(sources, printer) {
+  const logSourceStreams = sources.map(() => new Subject());
+  const sourceCount = sources.length;
+  const allLogsStream = merge(...logSourceStreams);
+
+  const processedLogsStream = allLogsStream.pipe(
+    scan(
+      (acc, log) => {
+        if (!log.data) {
+          acc.numUndrainedSources--;
+        } else {
+          acc.queue.enqueue(log);
+          if (!acc.sourcesInQueue[log.sourceIndex]) {
+            acc.sourcesInQueue[log.sourceIndex] = true;
+            acc.numSourcesInQueue++;
+          }
+        }
+        return acc;
+      },
+      {
+        queue: new MinPriorityQueue((log) => log.data.date.getTime()),
+        sourcesInQueue: new Array(sourceCount).fill(false),
+        numSourcesInQueue: 0,
+        numUndrainedSources: sourceCount,
+      }
+    )
+  );
+
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    let lastAcc;
+    processedLogsStream.subscribe({
+      next: (acc) => {
+        lastAcc = acc;
+        while (
+          acc.numSourcesInQueue === acc.numUndrainedSources &&
+          !acc.queue.isEmpty()
+        ) {
+          const log = acc.queue.dequeue();
+          printer.print(log.data);
+          acc.sourcesInQueue[log.sourceIndex] = false;
+          acc.numSourcesInQueue--;
+        }
+      },
+      error: reject,
+      complete: () => {
+        while (lastAcc && !lastAcc.queue.isEmpty()) {
+          const log = lastAcc.queue.dequeue();
+          printer.print(log.data);
+        }
+        printer.done();
+        console.log("Async sort complete.");
+        resolve();
+      },
+    });
+    for (let i = 0; i < sources.length; i++) {
+      fetchLogs(sources[i], i, logSourceStreams[i])();
+    }
   });
-};
+}
+
+function fetchLogs(source, index, sourceLogStream) {
+  return async function () {
+    while (true) {
+      const log = await source.popAsync();
+      if (!log) {
+        sourceLogStream.next({ data: null, sourceIndex: null });
+        sourceLogStream.complete();
+        break;
+      }
+      sourceLogStream.next({ data: log, sourceIndex: index });
+    }
+  };
+}
+
+module.exports = mergeAndPrintLogs;
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,35 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+const { MinPriorityQueue } = require("@datastructures-js/priority-queue");
 
-module.exports = (logSources, printer) => {
+function mergeAndPrintLogs(sources, printer) {
+  const queue = new MinPriorityQueue((item) => item.log.date.getTime());
+  sources.forEach((source, index) => {
+    const log = source.pop();
+    if (log) {
+      queue.push({ log, indexOfSource: index });
+    }
+  });
+  while (!queue.isEmpty()) {
+    const entry = queue.dequeue();
+    const log = entry.log;
+    const indexOfSource = entry.indexOfSource;
+    const source = sources[indexOfSource];
+
+    printer.print(log);
+
+    if (!source.drained) {
+      const nextLog = source.pop();
+      if (nextLog) {
+        entry.log = nextLog;
+        queue.push(entry);
+      }
+    }
+  }
+}
+
+module.exports = (sources, printer) => {
+  mergeAndPrintLogs(sources, printer);
+  printer.done();
   return console.log("Sync sort complete.");
 };
